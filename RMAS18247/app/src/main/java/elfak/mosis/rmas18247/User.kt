09b5baba4dfd468e2d873ecbd2b1d26b7e0728d9
Andjelija <mijajlovic.andjelija11@gmail.com>
2023-08-25 16:05:41 +0200

    Ponovo

--- RMAS18247/app/src/main/java/elfak/mosis/rmas18247/User.kt
+++
@@ -1,7 +0,0 @@
-package elfak.mosis.rmas18247
-
-data class User(val username:String?=null,
-                val firstName:String?= null,
-                val lastName:String?= null,
-                val phoneNumber: String?=null,
-                val profileImg: String?="")
