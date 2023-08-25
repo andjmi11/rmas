31afe543d6158cbb33f80ab1ae6620d30d0f748f
Andjelija <mijajlovic.andjelija11@gmail.com>
2023-08-25 15:33:23 +0200

    Mobilni projekat

---
+++ RMAS18247/app/src/main/java/elfak/mosis/rmas18247/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package elfak.mosis.rmas18247
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+}
