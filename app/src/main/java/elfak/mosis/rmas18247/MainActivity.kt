f465bff983cde63bf1b7b95978403640dde5edd8
Andjelija <mijajlovic.andjelija11@gmail.com>
2023-08-25 18:17:45 +0200

    C:/Program Files/Git/

---
+++ app/src/main/java/elfak/mosis/rmas18247/MainActivity.kt
@@ -0,0 +1,11 @@
+package elfak.mosis.rmas18247
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
